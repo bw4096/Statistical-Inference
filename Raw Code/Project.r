830362fd75a67ac39365bea4fc48624d536cd195
Bryan Willauer <bw4096@yahoo.com>
2020-04-24 13:21:10 -0400

    fixed some things in the code

--- Raw Code/Project.r
+++ Raw Code/Project.r
@@ -2,7 +2,7 @@ library(tidyverse)
 set.seed(1968)
 n <- 40
 lambda <- 0.2
-mean <- 1/lambda
+theoretical_mean <- 1/lambda
 sd <- 1/lambda
 variance <- (1/lambda)^2
 sem <- sd / sqrt(n)
@@ -18,9 +18,11 @@ sample_var <- var(sample)
 data <- data.frame(sample)
 data %>% ggplot(aes(sample)) +
         geom_histogram(alpha = 0.5, col = "Black", fill = "lightblue") +
-        geom_vline(size = 2,
-                xintercept = c(mean, sample_mean, variance, sample_var),
-                colour = c("Red", "Green", "Purple", "Yellow"),
-                lty = c(1, 2, 1, 2)) +
-        ggtitle("Figure 1: Histogram of 1000 rexp variables (lambda=0.2)")
+        geom_vline(size = 2,xintercept = theoretical_mean, colour = "Red", lty = 1) +
+        geom_vline(size = 2,xintercept = sample_mean, colour = "Green", lty = 2) +
+        geom_vline(size = 2,xintercept = variance, colour = "Purple", lty = 1) +
+        geom_vline(size = 2,xintercept = sample_var, colour = "Yellow", lty = 2) +
+        ggtitle("Figure 2: Histogram of 1000 rexp variables (lambda=0.2)") 
+
+# Histogram of means of 1000 40 sample exp variables
 
