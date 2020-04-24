56e7ac3d60b07be963adc85e0d99e4b8ac8ac931
Bryan Willauer <bw4096@yahoo.com>
2020-04-24 17:23:16 -0400

    Finalized

--- Raw Code/Project.r
+++ Raw Code/Project.r
@@ -22,7 +22,34 @@ data %>% ggplot(aes(sample)) +
         geom_vline(size = 2,xintercept = sample_mean, colour = "Green", lty = 2) +
         geom_vline(size = 2,xintercept = variance, colour = "Purple", lty = 1) +
         geom_vline(size = 2,xintercept = sample_var, colour = "Yellow", lty = 2) +
-        ggtitle("Figure 2: Histogram of 1000 rexp variables (lambda=0.2)") 
+        ggtitle("Figure 1: Histogram of 1000 rexp variables (lambda=0.2)") 
 
 # Histogram of means of 1000 40 sample exp variables
+lambda <- 0.2   # lambda
 
+n <- 40         # number of exponentials
+
+sims <- 1000    # number of simulations
+
+#Run simulations
+sim_exp <- replicate(sims, rexp(n, lambda))
+
+#Calc the means of the exponential simulations
+mu_xbar <- apply(sim_exp, 2, mean)
+
+data1 <- data.frame(mu_xbar)
+data1 %>% ggplot(aes(mu_xbar)) +
+        geom_histogram(alpha = 0.5, col = "Black", fill = "lightblue") +
+        geom_vline(size = 2,xintercept = theoretical_mean, colour = "Red", lty = 1) +
+        labs(x = "Simulation Means") +
+        geom_vline(size = 2,xintercept = mean(mu_xbar), colour = "Green", lty = 2) +
+        geom_vline(size = 2,xintercept = theoretical_mean + c(-1,1) * sem, colour = "Purple", lty = 1) +
+        geom_vline(size = 2,xintercept = mean(mu_xbar) + c(-1,1) * sd(mu_xbar), colour = "Yellow", lty = 2) +
+        ggtitle("Figure 2: Histogram of 1000 averages of 40 rexp variables (lambda=0.2)") 
+
+#Histogram with Normal Distribution Curve
+data1 %>% ggplot(aes(mu_xbar)) +
+        geom_histogram(alpha = 0.5, col = "Black", fill = "lightblue", aes(y = ..density..)) +
+        stat_function(fun = dnorm, args = list(mean = theoretical_mean, sd = sem)) +
+        labs(x = "Simulation Means") +
+        ggtitle("Figure 3: Histogram of 1000 averages of 40 rexp variables (lambda=0.2)") 
